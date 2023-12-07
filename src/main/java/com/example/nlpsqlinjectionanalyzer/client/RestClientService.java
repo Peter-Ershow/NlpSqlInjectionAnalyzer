f8138d62d2363b5534913a08a06acc54f54d555b
ershpeter <get82mot@mytum.de>
2023-12-07 14:54:10 +0100

    add models, logback and client

--- src/main/java/com/example/nlpsqlinjectionanalyzer/client/RestClientService.java
+++ src/main/java/com/example/nlpsqlinjectionanalyzer/client/RestClientService.java
@@ -1,5 +1,8 @@
 package com.example.nlpsqlinjectionanalyzer.client;
 
+import com.example.nlpsqlinjectionanalyzer.tokenizer.SQLSchemaTokenizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,6 +11,8 @@ public class RestClientService {
 
     private final RestTemplate restTemplate;
 
+    private static final Logger logger = LoggerFactory.getLogger(SQLSchemaTokenizer.class);
+
     public RestClientService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
@@ -15,5 +20,19 @@ public class RestClientService {
     public String getUsers() {
         String url = "http://localhost:8080/users";
         return restTemplate.getForObject(url, String.class);
+    }
+
+    public String runQuery(String input) {
+        StringBuilder allInfo = new StringBuilder();
+        String url = "http://localhost:8080/users/" + input;
+        logger.info("Calling safe");
+        String result = restTemplate.getForObject(url, String.class);
+        logger.info("Received :" + result);
+        allInfo.append("Safe Call: " + result);
+        logger.info("Calling unsafe");
+        String resultUnsafe = restTemplate.getForObject(url + "' OR '1'='1", String.class);
+        logger.info("Received Unsafe: " + resultUnsafe);
+        allInfo.append(resultUnsafe);
+        return resultUnsafe;
     }
 }
--- src/main/java/com/example/nlpsqlinjectionanalyzer/tokenizer/SQLSchemaTokenizer.java
+++ src/main/java/com/example/nlpsqlinjectionanalyzer/tokenizer/SQLSchemaTokenizer.java
@@ -1,24 +1,50 @@
 package com.example.nlpsqlinjectionanalyzer.tokenizer;
 
+import com.example.nlpsqlinjectionanalyzer.client.RestClientService;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.tokenize.SimpleTokenizer;
 import opennlp.tools.util.Span;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
 @Component
 public class SQLSchemaTokenizer {
 
-    public String getEntities(String text) throws IOException {
-        // Load the NER model
-        TokenNameFinderModel model;
+    @Autowired
+    private RestClientService restClientService;
 
+    private static final Logger logger = LoggerFactory.getLogger(SQLSchemaTokenizer.class);
+
+    public String getEntities(String text){
+        String outputNames = generateEntities(text,"en-ner-person.bin");
+
+        String outputLocations  = generateEntities(text, "en-ner-location.bin");
+
+        String outputOrg  = generateEntities(text, "en-ner-organization.bin");
+
+        String outputDates  = generateEntities(text, "en-ner-date.bin");
+
+        return outputLocations +
+                System.lineSeparator() +
+                outputNames +
+                System.lineSeparator() +
+                outputOrg +
+                System.lineSeparator() +
+                outputDates;
+    }
+
+    private String generateEntities(String text, String file) {
+        TokenNameFinderModel model;
         try (InputStream modelIn = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("en-ner-person.bin")) {
+                .getResourceAsStream(file)) {
 
             if (modelIn == null) {
                 throw new IllegalArgumentException("File not found!");
@@ -26,25 +52,43 @@ public class SQLSchemaTokenizer {
 
             model = new TokenNameFinderModel(modelIn);
 
-        // Create a NameFinder using the model
-        NameFinderME nameFinder = new NameFinderME(model);
+            // create a NameFinder using the model
+            NameFinderME nameFinder = new NameFinderME(model);
 
-        // Tokenize the text
-        String[] tokens = SimpleTokenizer.INSTANCE.tokenize(text);
+            // tokenize the text
+            String[] tokens = SimpleTokenizer.INSTANCE.tokenize(text);
+            Span[] nameSpans = nameFinder.find(tokens);
 
-        // Find names
-        Span[] nameSpans = nameFinder.find(tokens);
+            // remove duplicates
+            Set<Span> distinctSpans = new TreeSet<>(Span::compareTo);
+            distinctSpans.addAll(Arrays.asList(nameSpans));
+            Span[] distinctNameSpans = distinctSpans.toArray(new Span[0]);
 
-        // Print names
-        StringBuilder output = new StringBuilder();
-        for (Span s : nameSpans) {
-            output.append("Entity: ").append(tokens[s.getStart()]).append("\n");
-        }
+            // print names
+            StringBuilder output = new StringBuilder();
+            output.append("Empty URL: ").append(runCalls(""));
+            for (Span s : distinctNameSpans) {
+                String possibleInput = tokens[s.getStart()].replaceAll(" ", "").toLowerCase();
+                String result = runCalls(possibleInput);
+                if(!result.isEmpty()) {
+                    output.append(result).append(", Next Call: ");
+                }
+            }
+
             return output.toString();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
+    }
 
-        return "";
+    private String runCalls(String text){
+        try {
+            logger.info("Doing call for " + text);
+            return restClientService.runQuery(text);
+        }
+        catch (Exception e){
+            return "";
+        }
     }
 }
